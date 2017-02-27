baa750900768df97c5eac334761b9a06b6123b7f
viyozc <chisny@163.com>
2017-02-27 22:42:41 +0800

    file ready

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+var app = angular.module('nearby',['ionic'])
+app.config(function($stateProvider,$urlRouterProvider,$ionicConfigProvider){
+	$stateProvider.state('start',{
+		url:'xxx',
+		templateUrl:'xxx'
+	})
+	
+})
